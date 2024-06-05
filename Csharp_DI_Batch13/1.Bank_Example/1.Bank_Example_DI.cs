e9291f4d685f764f76d040bfcd8025b70dd471e7
Madn <madan.patakota@gmail.com>
2024-06-05 11:33:57 +0530

    Add project files.

---
+++ Csharp_DI_Batch13/1.Bank_Example/1.Bank_Example_DI.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDINamespace
+{
+    //Angular // APIs (Dependency injection patern) 10 days
+    public interface IAccount
+    {
+        decimal GetBalance();
+    }
+
+    public class BankAccount : IAccount
+    {
+        private decimal _balance;
+        public BankAccount(decimal balance)
+        {
+            _balance = balance;
+        }
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+    }
+
+
+    // This is real time....
+    public class BankService
+    {
+        private IAccount _account;
+        public BankService(IAccount account)
+        {
+            _account = account;
+        }
+
+        public void DisplayBalance()
+        {
+            //interface i will pass the BankAccount related ref 
+            //IAccount account = new BankAccount();
+            //account.GetBalance();
+            decimal balance = _account.GetBalance();
+            Console.WriteLine($"Account Balance : {balance}");
+        }
+    }
+
+
+    //f10 and f11 
+    public class Program
+    {
+        static void Main()
+        {
+            IAccount account = new BankAccount(1000);
+            account.GetBalance();
+
+            //account is holding the BankAccount reference.
+            BankService bankService = new BankService(account);
+            bankService.DisplayBalance();
+        }
+    }
+
+
+
+}
---
+++ Csharp_DI_Batch13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_DI_Batch13
+{
+    class Class1
+    {
+        private int _id;
+        private string _name;
+        public Class1(int k , string j)
+        {
+            _id = k;
+            _name = j;
+        }
+
+        public void printMessage()
+        {
+            Console.WriteLine($"{_id} , {_name}");
+        }
+        public int getnumber()
+        {
+            return 10;
+        }
+
+        public int getnumber2()
+        {
+            return 20;
+        }
+    }
+
+
+    class Class2
+    {
+        static void Main()
+        {
+            Class1 c1 = new Class1(0,"");
+            c1.getnumber();
+        }
+    }
+
+
+    class Class3
+    {
+        static void Main()
+        {
+            Class1 c1 = new Class1(0, "");
+            c1.getnumber2();
+        }
+    }
+
+
+}
